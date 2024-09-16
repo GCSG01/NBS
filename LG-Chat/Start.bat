143857cf6cf60903e54bbef11a8fc7241721e9bc
GCSG01 <GCSG01@outlook.com>
2024-09-16 16:15:07 +0800

    移库

--- LG-Chat/Start.bat
+++
@@ -1,7 +0,0 @@
-pip install flask -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install requests -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install loguru -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install openai -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install windows_toasts  -i https://pypi.tuna.tsinghua.edu.cn/simple
-start a.bat
-start b.bat
